a9009bc4426719ec46b2a095f4d22019d07bd85c
Matthew Sharpe <mtshar74@thomasmore.edu>
2025-12-18 18:37:30 -0500

    Add project files.

---
+++ PythonDataStructures/PythonDataStructures.py
@@ -0,0 +1,28 @@
+
+# practicing some searching and sorting algorithms.
+
+def bubbleSort(arr):
+    flag = True  
+    while flag:
+        flag = False
+        for x in range(1, len(arr)):
+            if arr[x] < arr[x - 1]:
+                flag = True      # flag is reset to True, allowing the loop to continue to run
+                arr[x], arr[x-1] = arr[x-1], arr[x]
+            
+
+
+def binarySearch(arr, target):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        midpoint = (left + right) // 2 
+        if arr[midpoint] == target:
+            return midpoint
+        elif arr[midpoint] < target:
+            left = midpoint + 1     # only concerned with the right hand side
+        else:
+            right = midpoint - 1 # only concerned with the left hand side
+
+    return -1 # target not found
+
+
---
+++ PythonDataStructures/graphs.py
@@ -0,0 +1,4 @@
+
+# Edge List: [[from, to], [from, to], [n , n]]
+# Adjacency List: (hashmap) {nodes value: array of all nodes we can travel to}
+
