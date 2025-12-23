bcb3a9e915be0671f3b264717ff0db4246117718
Matthew Sharpe <matthewsharpe49@icloud.com>
2025-12-23 02:15:24 -0500

    Adding merge sort

--- PythonDataStructures/PythonDataStructures.py
+++ PythonDataStructures/PythonDataStructures.py
@@ -40,3 +40,42 @@ def heap_sort(arr):
 arr = [4, 10, 3, 5, 1]
 heap_sort(arr)
 print("Sorted array:", arr)
+
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    # Splitting the list in half
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+
+    # Merging the sorted halves
+    return merge(left, right)
+
+
+def merge(left, right):
+    result = []
+    i = j = 0
+
+    # Comparing elements from both lists
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+ 
+    result.extend(left[i:])
+    result.extend(right[j:])
+
+    return result
+
+
+# Example
+numbers = [8, 3, 5, 2, 7, 4]
+sorted_numbers = merge_sort(numbers)
+print(sorted_numbers)
