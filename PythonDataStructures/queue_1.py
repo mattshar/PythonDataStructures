9bd815b38aaa4e736e3ab6bf6214237caa439445
Matthew Sharpe <matthewsharpe49@icloud.com>
2026-01-17 21:03:17 -0500

    queue operations

--- PythonDataStructures/queue_1.py
+++ PythonDataStructures/queue_1.py
@@ -6,5 +6,23 @@ q.append(1)      #
 q.append(2)
 q.append(3)
 
-item = q.popleft()  
-print(item)  # 1
+q.extend([4, 5, 6])
+q.extendleft([-2, -1])
+
+first = q.popleft()
+last = q.pop()
+
+q.rotate(2)
+q.rotate(-1)
+
+q.insert(2, 99)
+q.remove(3)
+
+length = len(q)
+contains = 4 in q
+
+print(first)
+print(last)
+print(length)
+print(contains)
+print(q)
