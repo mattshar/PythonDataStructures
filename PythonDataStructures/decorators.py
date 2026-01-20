96db0c45e34af6a9f563b064d67dedcaa7be4d79
Matthew Sharpe <matthewsharpe49@icloud.com>
2026-01-19 20:46:52 -0500

    more decorators

--- PythonDataStructures/decorators.py
+++ PythonDataStructures/decorators.py
@@ -9,3 +9,25 @@ def time_it(func):
         print(f"{func.__name__} took {time.time() - start:.2f}s")
         return result
     return wrapper
+
+
+def count_calls(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        wrapper.calls += 1
+        print(f"{func.__name__} has been called {wrapper.calls} times")
+        return func(*args, **kwargs)
+
+    wrapper.calls = 0
+    return wrapper
+
+
+@count_calls
+def say_hello(name):
+    print(f"Hello, {name}!!!!!!!!!!")
+
+
+#  Random Names
+say_hello("Alice")
+say_hello("Bob")
+say_hello("Charlie")
