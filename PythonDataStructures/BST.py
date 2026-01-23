60cb7e950fd8ad4ef4168e830f6b9fb0ae8a86be
Matthew Sharpe <matthewsharpe49@icloud.com>
2026-01-22 21:25:33 -0500

    binary search tree

---
+++ PythonDataStructures/BST.py
@@ -0,0 +1,29 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        if self.root is None:
+            self.root = Node(value)
+        else:
+            self._insert_recursive(self.root, value)
+
+    def _insert_recursive(self, current, value):
+        if value < current.value:
+            if current.left is None:
+                current.left = Node(value)
+            else:
+                self._insert_recursive(current.left, value)
+        elif value > current.value:
+            if current.right is None:
+                current.right = Node(value)
+            else:
+                self._insert_recursive(current.right, value)
+
+
