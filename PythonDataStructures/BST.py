e87970a1135662e9ecc3f497baf9eddae4fa8255
Matthew Sharpe <matthewsharpe49@icloud.com>
2026-01-26 21:53:45 -0500

    balanced BST

--- PythonDataStructures/BST.py
+++ PythonDataStructures/BST.py
@@ -27,6 +27,29 @@ class BST:
                 self._insert_recursive(current.right, value)
 
 
+    def balance(self):
+        # 1. Turn the tree into a simple sorted array / list of values
+        vals = []
+        def pull_vals(node):
+            if node:
+                pull_vals(node.left)
+                vals.append(node.value)
+                pull_vals(node.right)
+        
+        pull_vals(self.root)
+
+        # 2  Separate Function to rebuild a balanced tree from that list
+        def build(elements):
+            if not elements:
+                return None
+            mid = len(elements) // 2
+            root = Node(elements[mid]) # Middle becomes the new parent
+            root.left = build(elements[:mid]) # Left half becomes left child
+            root.right = build(elements[mid+1:]) # Right half becomes right child
+            return root
+
+        self.root = build(vals)
+
 
 
 
